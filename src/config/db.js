0a245e2ba76e77ff4a563d7fa0d969e766ba29a9
susanxtha1 <susanxtha1@gmail.com>
2026-08-25 20:36:22 +0545

    Fix Prisma client generation

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
-
 import { PrismaClient } from "@prisma/client";
 
+
+
+
 const prisma = new PrismaClient({
+
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "info", "warn", "error"]
       : ["error"],
 });
+
 const connectDB = async () => {
   try {
     await prisma.$connect();
@@ -16,6 +20,7 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
 const disconnectDB = async () => {
   await prisma.$disconnect();
 };
