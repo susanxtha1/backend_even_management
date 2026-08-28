8268f21155c81ded0c941545d0cc1664078d7d96
susanxtha1 <susanxtha1@gmail.com>
2026-08-28 09:00:55 +0545

    Readme updated

--- backend/src/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/hello", (req, res) => {
-  res.json({ message: "Welcome to the event route!" });
-});
-
-export default router;
