3d26e9aff2b42e293469f386332f55336a83933d
susanxtha1 <susanxtha1@gmail.com>
2026-09-02 11:59:33 +0545

    slug validation

--- src/schema/categorySchema.js
+++ src/schema/categorySchema.js
@@ -6,6 +6,11 @@ export const createCategorySchema = z.object({
     .min(2, "Category name must be at least 2 characters")
     .max(50, "Category name must not exceed 50 characters"),
   description: z.string().optional(),
+  slug: z
+    .string()
+    .min(2, "Slug must be at least 2 characters")
+    .max(100, "Slug must not exceed 100 characters")
+    .optional(),
 });
 
 export const updateCategorySchema = createCategorySchema.partial();
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import eventRoutes from "./routes/eventRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import venueRoutes from "./routes/venueRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
+
 import cors from "cors";
 
 connectDB();
